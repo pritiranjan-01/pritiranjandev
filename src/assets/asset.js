5b7bf3f76398af41323f70ce078fea9c718f4497
Pritiranjan Mohanty <pritiranjan.mohanty2003@gmail.com>
2026-03-02 17:15:24 +0530

    fixed case sensitivity issue in imports

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -2,13 +2,13 @@ import pp from "./profile_picture.png";
 import collegLogo from "./tat.png";
 import collegLogo2 from "./mcnl.jpg";
 import logo from "./logo.jpeg";
-import BM from "./ProjectDemo/BankManagementSystem.mp4";
-import BS from "./ProjectDemo/BillingSystem.mp4";
-import US from "./ProjectDemo/URLShortner.mp4";
-import GS from "./ProjectDemo/GlobalWeatherService.mp4";
-import SM from "./ProjectDemo/StudentManagementPro.mp4";
-import TF from "./ProjectDemo/TodoFlow.mp4";
-import EC from "./ProjectDemo/ECommerce.png";
+import BM from "./ProjectDemo/Bankmanagementsystem.mp4";
+import BS from "./ProjectDemo/Billingsystem.mp4";
+import US from "./ProjectDemo/Urlshortner.mp4";
+import GS from "./ProjectDemo/Globalweatherservice.mp4";
+import SM from "./ProjectDemo/Studentmanagementpro.mp4";
+import TF from "./ProjectDemo/Todoflow.mp4";
+import EC from "./ProjectDemo/Ecommerce.png";
 
 export const assets = {
   profile: pp,
