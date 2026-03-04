3768188f043decfd699e0adb74715baa6fcb641e
Pritiranjan Mohanty <pritiranjan.mohanty2003@gmail.com>
2026-03-04 10:00:01 +0530

    fixed navbar selected tab styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Sun, Moon } from "lucide-react";
 import { assets } from "../assets/asset";
 
 const navLinkClasses =
-  "text-xs sm:text-sm md:text-base font-medium tracking-wide px-2 sm:px-3 py-1 rounded-full hover:bg-light-bgSecondary hover:text-accent-light dark:hover:bg-dark-bgSecondary dark:hover:text-accent-dark transition-colors relative";
+  "text-xs sm:text-sm md:text-base font-medium tracking-wide px-2 sm:px-3 py-1 rounded-full transition-colors relative";
 
 const activeNavLinkClasses =
   "after:absolute after:bottom-0 after:left-1/2 after:-translate-x-1/2 after:w-full after:h-0.5 after:bg-black dark:after:bg-white after:origin-center after:animate-slideUnderline";
